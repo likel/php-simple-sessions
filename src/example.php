7aeb15ff18bce2114fb528d247ad8c292d121427
Liam Kelly <32558851+likel@users.noreply.github.com>
2017-10-09 22:03:22 +1000

    Docblock update

--- src/example.php
+++ src/example.php
@@ -1,17 +1,12 @@
 <?php
 /**
- * The index file, we can run our code from here
+ * Example usage of the php-simple-sessions package
  *
- * A program which prints numbers from 1 to 100 with the following conditions:
- *    multiples of 3 print "Fizz" instead of the number
- *    multiples of 5 print "Buzz" instead of the number
- *    multiples of 3 & 5 print "FizzBuzz"
- *
- * @package     fizz-buzz
+ * @package     php-simple-sessions
  * @author      Liam Kelly <https://github.com/likel>
  * @copyright   2017 Liam Kelly
- * @license     https://github.com/likel/fizz-buzz/blob/master/LICENSE GPL-3.0 License
- * @link        https://github.com/likel/fizz-buzz
+ * @license     MIT License <https://github.com/likel/fizz-buzz/blob/master/LICENSE>
+ * @link        https://github.com/likel/php-simple-sessions
  * @version     1.0.0
  */
 
