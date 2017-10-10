d049877a0482483268052398335eec96216357d2
Liam Kelly <32558851+likel@users.noreply.github.com>
2017-10-10 18:04:05 +1000

    Initial test commit
    Created test directory and SessionHandlerTest.php

---
+++ test/SessionHandlerTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PHPUnit tests for models/Session/Handler.php
+ *
+ * @package     php-simple-sessions
+ * @author      Liam Kelly <https://github.com/likel>
+ * @copyright   2017 Liam Kelly
+ * @license     MIT License <https://github.com/likel/php-simple-sessions/blob/master/LICENSE>
+ * @link        https://github.com/likel/php-simple-sessions
+ * @version     1.0.0
+ */
+use PHPUnit\Framework\TestCase;
+
+// Require the autoloader to load the models when required
+require_once(__DIR__ . '/../src/autoload.php');
+
+final class SessionHandlerTest extends TestCase
+{
+    /**
+     * Test for the constructor outcomes
+     */
+    public function testConstruct()
+    {
+
+    }
+}
